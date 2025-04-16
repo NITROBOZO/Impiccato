3a2e29deb9bd416a598b0ad58ffa01394f60a794
PNRR_STUDENTI <PNRR_STUDENTI@DESKTOP-3LML4DT.localdomain>
2025-04-16 15:32:56 +0200

    sigma

--- impiccato/src/model/Impiccato.java
+++ impiccato/src/model/Impiccato.java
@@ -1,8 +1,10 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Impiccato {
 	private String parolaSegreta;
-	private StringBuilder parolaAttuale;
+	private String parolaAttuale;
 	private int tentativiRimanenti;
 	
 	public Impiccato() {
@@ -11,7 +13,15 @@ public class Impiccato {
 	
 	public void setParolaSegreta(String parolaSegreta) {
 		this.parolaSegreta = parolaSegreta.toUpperCase();
-		this.parolaAttuale = new StringBuilder("_".repeat(parolaSegreta.length()));
+		this.parolaAttuale = new String();
+		for(int i=0;i<parolaSegreta.length();i++) {
+			if(parolaSegreta.charAt(i)==' ') {
+				this.parolaAttuale+=' ';
+			}
+			else {
+				this.parolaAttuale+='_';
+			}
+		}
 	}
 	
 	
@@ -26,13 +36,38 @@ public class Impiccato {
 	public int getTentativiRimanenti() {
 		return tentativiRimanenti;
 	}
-	
+	public boolean verificaParola(String parola) {
+		boolean parolaTrovata = false;
+		String[] parole = parola.split("\\s+");
+		String[] paroleSegrete = parolaSegreta.split("\\s+");
+		String[] paroleAttuali = parolaAttuale.split("\\s+");
+		for(int i=0;i<parole.length;i++) {
+			for(int j=0;j<paroleSegrete.length;j++) {
+				if(parole[i].equals(paroleSegrete[j])) {
+					paroleAttuali[j]=parole[i];
+					parolaTrovata = true;
+					break;
+				}
+			}
+		}
+		
+		if (!parolaTrovata && tentativiRimanenti > 0) {
+			tentativiRimanenti-=2;
+			if(tentativiRimanenti<0) {
+				tentativiRimanenti=0;
+			}
+		}
+		return parolaTrovata;
+		
+	}
 	public boolean verificaLettera(char lettera) {
 		boolean letteraTrovata = false;
 		
 		for (int i = 0; i < parolaSegreta.length(); i++) {
 			if (parolaSegreta.charAt(i) == lettera) {
-				parolaAttuale.setCharAt(i, lettera);
+				StringBuilder p = new StringBuilder(parolaAttuale);
+				p.setCharAt(i, lettera);
+				parolaAttuale = p.toString();
 				letteraTrovata = true;
 			}
 		}
